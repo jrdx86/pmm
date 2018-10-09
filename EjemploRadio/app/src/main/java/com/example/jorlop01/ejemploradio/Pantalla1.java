ad925b05978d54b0c87d5d37d6c1b784c4ad86b2
Jordi <jrdx86>
2018-10-09 13:08:39 +0200

    EjemploRadio: Añadidas mejoras texto y toast's

--- EjemploRadio/app/src/main/java/com/example/jorlop01/ejemploradio/Pantalla1.java
+++ EjemploRadio/app/src/main/java/com/example/jorlop01/ejemploradio/Pantalla1.java
@@ -2,28 +2,43 @@ package com.example.jorlop01.ejemploradio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Pantalla1 extends AppCompatActivity {
+
+
+    RadioGroup rg;
+    TextView lblMensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla1);
 
-        final TextView lblMensaje = (TextView) findViewById(R.id.LblSeleccion);
-        final RadioGroup rg = (RadioGroup) findViewById(R.id.gruporb);
+
+
+
+        lblMensaje = (TextView) findViewById(R.id.LblSeleccion);
+        rg = (RadioGroup) findViewById(R.id.gruporb);
         rg.clearCheck();
 
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                lblMensaje.setText("Opcion seleccionada: " + checkedId);
 
-              /* Mejora el texto informativo
-                String textoOpcion="";
-                if( group.getCheckedRadioButtonId()==R.id.radio1)...... */
-            }
+
+                if( group.getCheckedRadioButtonId()==R.id.radio1) {
+                    lblMensaje.setText("Has elegido la opcion 1");
+                    Toast.makeText(Pantalla1.this, lblMensaje.getText(), Toast.LENGTH_SHORT).show();
+                }
+                if( group.getCheckedRadioButtonId()==R.id.radio2) {
+                    lblMensaje.setText("Has elegido la opcion 2");
+                    Toast.makeText(Pantalla1.this, lblMensaje.getText(), Toast.LENGTH_SHORT).show();
+                }
+                }
         });
     }
+
 }
